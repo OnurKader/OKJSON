03abe39e747b4d6d07f3d7f0ed2dd5bd65f2b461
OnurKader <onurkader@protonmail.com>
2020-06-04 20:27:43 +0300

    Switched to `dev`
    > Added some simple array/vector/pair formatters

--- src/JSON.cpp
+++ src/JSON.cpp
@@ -1,7 +1,22 @@
-#include <fmt/format.h>
+#include "Utility.hpp"
 
-int main(int argc, char** argv)
+#include <fmt/color.h>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fstream>
+#include <string>
+
+int main()
 {
-	fmt::print("Hello There!\n");
+	std::ifstream test_json {"test.json"};
+	if(!test_json)
+	{
+		fmt::print(stderr, "Couldn't open file\n");
+		return 1;
+	}
+	std::string json_data {std::istream_iterator<char>(test_json), std::istream_iterator<char>()};
+
+	fmt::print("{}\n", json_data);
+
 	return 0;
 }
